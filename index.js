3d1deb9385e88607ebd639a02535090d6bb1875d
Enes <aydontloss@gmail.com>
2024-03-17 02:31:18 +0200

    NPX command removed npm run setup created

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ import ora from 'ora';
 const spinner = ora({ text: 'Setting up ExWeiv Wix-React compilers and folders with pre-built examples...', color: 'magenta' }).start();
 
 try {
-    await execa('mkdir', ['React']);
-    await execa('git', ['clone', 'git@github.com:ExWeiv/wix-react.git', './React']);
-    await execa('npm', ['install'], { cwd: './React' })
-    await execa('mkdir', ['src/public/components'])
-    await execa('mkdir', ['src/public/css'])
-    await execa('mkdir', ['src/public/css/files'])
+    await execa('mkdir', ['../src/public/components'])
+    await execa('mkdir', ['../src/public/css'])
+    await execa('mkdir', ['../src/public/css/files'])
     spinner.succeed('ExWeiv Wix-React is ready!');
 } catch (err) {
     console.error(chalk.red(`Error while setting up ExWeiv Wix-React: ${err}`));
