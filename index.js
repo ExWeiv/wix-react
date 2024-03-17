ac9e80b801e033028f554a30541181c235c88c18
Enes <aydontloss@gmail.com>
2024-03-17 02:37:35 +0200

    setup is improved

--- index.js
+++ index.js
@@ -3,13 +3,28 @@
 import { execa } from 'execa';
 import chalk from 'chalk';
 import ora from 'ora';
+import fs from 'fs';
 
 const spinner = ora({ text: 'Setting up ExWeiv Wix-React compilers and folders with pre-built examples...', color: 'magenta' }).start();
 
+const folders = [
+    '../src/public/components',
+    '../src/public/css',
+    '../src/public/css/files'
+]
+
 try {
-    await execa('mkdir', ['../src/public/components'])
-    await execa('mkdir', ['../src/public/css'])
-    await execa('mkdir', ['../src/public/css/files'])
+    for (const folder of folders) {
+        fs.access(folder, fs.constants.F_OK, async (err) => {
+            if (err) {
+                const printT = folder.slice(3);
+                console.log(chalk.hex('#fcba03')(`${printT} folder already exists so skipping this`))
+            }
+
+            await execa('mkdir', [folder])
+        })
+    }
+
     spinner.succeed('ExWeiv Wix-React is ready!');
 } catch (err) {
     console.error(chalk.red(`Error while setting up ExWeiv Wix-React: ${err}`));
