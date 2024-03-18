cb4e556fb44503353070301d2b6da066b13333c8
Enes <aydontloss@gmail.com>
2024-03-18 05:45:56 +0200

    npm run setup BUG Fixed

--- index.js
+++ index.js
@@ -12,37 +12,36 @@ const folders = [
     '../src/public/css',
     '../src/public/css/files',
     '../src/public/custom-elements'
-]
+];
 
-const clearFolders = [...folders];
+async function createFolderIfNotExists(folder) {
+    try {
+        await fs.promises.stat(folder);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await execa('mkdir', [folder]);
+        } else {
+            console.error(chalk.red(`Error: ${err}`));
+        }
+    }
+}
 
-try {
+(async () => {
+    const clearFolders = [];
+
     for (const folder of folders) {
-        await fs.stat(folder, async (err, stats) => {
-            if (err) {
-                if (err.code === 'ENOENT') {
-                    await execa('mkdir', [folder])
-                } else {
-                    console.error(chalk.red(`Error: ${err}`));
-                }
-            } else {
-                if (await stats.isDirectory()) {
-                    clearFolders.pop();
-                    const printT = folder.slice(3);
-                    console.log(chalk.hex('#fcba03')(`${printT} folder already exists so skipping this`))
-                }
-            }
-        })
+        try {
+            await createFolderIfNotExists(folder);
+            clearFolders.push(folder);
+        } catch (err) {
+            console.error(chalk.red(`Error while setting up ExWeiv Wix-React: ${err}`));
+        }
     }
 
-    console.log("totalSkip", clearFolders)
-
-    if (clearFolders.length === 0) {
-        spinner.clear();
-        console.log(chalk.hex('#fcba03')('All folders are already exist so nothing was created!'));
+    if (clearFolders.length === folders.length) {
+        spinner.stop();
+        console.log(chalk.hex('#fcba03')('All folders already exist, so nothing was created!'));
     } else {
         spinner.succeed('ExWeiv Wix-React is ready!');
     }
-} catch (err) {
-    console.error(chalk.red(`Error while setting up ExWeiv Wix-React: ${err}`));
-}
+})();
